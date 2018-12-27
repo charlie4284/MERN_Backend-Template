c6ff5168fe9f039e3c45c4c3aedd390ea21eb5f4
YangSoo Yoon <yanks.yoon@gmail.com>
2018-12-27 23:37:05 +0800

    Mern
    Stack

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // mongoURI: `mongodb://localhost:${portNum}/${databaseName}?authSource=${authSourceName} --username ${username}`
+    mongoURI: 'mongodb://localhost/default'
+}
